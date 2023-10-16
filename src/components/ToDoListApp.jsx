2c5d630501d47ad255705bae7b75b8f703eb292a
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-16 15:28:17 +0100

    fonction add

--- src/components/ToDoListApp.jsx
+++ src/components/ToDoListApp.jsx
@@ -6,53 +6,55 @@ import { BsFillBarChartFill } from 'react-icons/bs';
 export default function ToDoListApp() {
 
     const [taches, setTaches] = useState([
-        {desricption:'Réviser HTML/CSS', completed:true},
-        {desricption:'Réviser mongodb', completed:false},
-        {desricption:'Réviser Javascript', completed:true}
+        { desricption: 'Réviser HTML/CSS', completed: true },
+        { desricption: 'Réviser mongodb', completed: false },
+        { desricption: 'Réviser Javascript', completed: true }
     ])
     const [task, setTask] = useState('');
 
     const addTache = (e) => {
-        if(task.trim()==='')
+        if (task.trim() === '') {
             alert('Description de tache vide');
-        else {
-            if(taches.filter(t => t.desricption.toUpperCase()===task.toUpperCase()).length>0){
-                alert('Tache existe deja');
-                return
-            }
-            setTaches(taches => [...taches, {desricption:task, completed:false }]);
-            setTask('')
-            document.getElementById('task').focus()
+            return
         }
+
+        if (taches.filter(t => t.desricption.toUpperCase() === task.toUpperCase()).length > 0) {
+            alert('Tache existe deja');
+            return
+        }
+
+        setTaches(taches => [...taches, { desricption: task, completed: false }]);
+        setTask('')
+        document.getElementById('task').focus()
     }
 
-  return (
-    <div className='content'>
-        <div>
-            <fieldset className='ajout'>
-                <legend>Ajout</legend>
-                <input type="text" name="task" id="task" value={task} onChange={(e) => setTask(e.target.value)} placeholder='saisir descrition ...'/>
-                <button onClick={addTache}><MdAdd /> Add</button>
-            </fieldset>
-        </div>
-        <div>
-            <div className='info'>
-                <BsFillBarChartFill />
-                <div>
-                    {taches.length} Taches <br />
-                    {taches.filter(t => t.completed).length} Terminé <br />
-                    {taches.filter(t => !t.completed).length} En cours
+    return (
+        <div className='content'>
+            <div>
+                <fieldset className='ajout'>
+                    <legend>Ajout</legend>
+                    <input type="text" name="task" id="task" value={task} onChange={(e) => setTask(e.target.value)} placeholder='saisir descrition ...' />
+                    <button onClick={addTache}><MdAdd /> Add</button>
+                </fieldset>
+            </div>
+            <div>
+                <div className='info'>
+                    <BsFillBarChartFill />
+                    <div>
+                        {taches.length} Taches <br />
+                        {taches.filter(t => t.completed).length} Terminé <br />
+                        {taches.filter(t => !t.completed).length} En cours
+                    </div>
+                </div>
+                <div className='list-tasks'>
+                    {taches.map((t, index) => <div key={index} className='task'>
+                        <h3>{index + 1} - {t.desricption}</h3>
+                        {
+                            t.completed ? <AiOutlineCheck /> : <MdOutlinePending />
+                        }
+                    </div>)}
                 </div>
             </div>
-            <div className='list-tasks'>
-                {taches.map((t, index) => <div key={index} className='task'>
-                    <h3>{index+1} - {t.desricption}</h3>
-                    {
-                        t.completed ? <AiOutlineCheck /> : <MdOutlinePending />
-                    }   
-                </div> )}
-            </div>  
         </div>
-    </div>
-  )
+    )
 }
