56e53919d4c94f9b99228ebcf3b3a5e5140ed624
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-16 14:48:47 +0100

    partie 2

--- src/components/ToDoListApp.jsx
+++ src/components/ToDoListApp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { AiOutlineCheck } from 'react-icons/ai';
-import { MdOutlinePending } from 'react-icons/md';
+import { MdOutlinePending, MdAdd } from 'react-icons/md';
 
 export default function ToDoListApp() {
 
@@ -9,15 +9,29 @@ export default function ToDoListApp() {
         {desricption:'Réviser mongodb', completed:false},
         {desricption:'Réviser Javascript', completed:false}
     ])
+    const [task, setTask] = useState();
+
+    const addTache = (e) => {
+        setTaches(taches => [...taches, {desricption:task, completed:true }]);
+    }
 
   return (
-    <div>
-        {taches.map(t => <div className='task'>
-            <h3>{t.desricption}</h3>
-            {
-                t.completed ? <AiOutlineCheck /> : <MdOutlinePending />
-            }   
-        </div> )}
+    <div className='content'>
+
+        <fieldset>
+            <legend>Ajout</legend>
+            <input type="text" name="task" id="task" value={task} onChange={(e) => setTask(e.target.value)} />
+            <button onClick={addTache}><MdAdd /> Add</button>
+        </fieldset>
+
+        <div className='list-tasks'>
+            {taches.map((t, index) => <div key={index} className='task'>
+                <h3>{t.desricption}</h3>
+                {
+                    t.completed ? <AiOutlineCheck /> : <MdOutlinePending />
+                }   
+            </div> )}
+        </div>
     </div>
   )
 }
